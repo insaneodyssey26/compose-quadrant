e6c740d87c76cb95df572e2efcfafac55b48be67
Masum <hydrapikachu29@gmail.com>
2025-06-07 20:40:41 +0530

    Added first card

--- app/src/main/java/com/masum/composequadrant/MainActivity.kt
+++ app/src/main/java/com/masum/composequadrant/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.masum.composequadrant.ui.theme.ComposeQuadrantTheme
 
@@ -27,6 +32,20 @@ class MainActivity : ComponentActivity() {
 
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun QuadrantScreen() {
+    Column(Modifier.fillMaxWidth()) {
+        Row(Modifier.weight(1f)) {
+            Cards(
+                title = "Text composable",
+                description = "Displays text and follows Material Design guidelines.",
+                Modifier.weight(1f),
+                backgroundColor = Color(0xFFEADDFF)
+            )
         }
     }
 }
