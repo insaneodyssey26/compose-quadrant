a3ed9305b13a583cb9ad5f2ad58c7db4706916db
Masum <hydrapikachu29@gmail.com>
2025-06-09 21:25:42 +0530

    Removed unused imports

--- app/src/main/java/com/masum/composequadrant/MainActivity.kt
+++ app/src/main/java/com/masum/composequadrant/MainActivity.kt
@@ -11,9 +11,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,7 +20,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.masum.composequadrant.ui.theme.ComposeQuadrantTheme
 
